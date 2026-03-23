2a94641bca4c84865ba0867e034b5d9b94d3ea1b
neonwatty <jermwatt@gmail.com>
2026-03-23 14:25:36 -0700

    chore: add validate script for full local quality check

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'security', 'docs', 'test', 'chore', 'refactor', 'ci', 'style', 'build', 'revert']],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'perf', 'security', 'docs', 'test', 'chore', 'refactor', 'ci', 'style', 'build', 'revert'],
+    ],
   },
 }
