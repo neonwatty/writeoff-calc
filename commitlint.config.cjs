67c93b2f0bd594b8e674f8ca775eff42f8d98b39
neonwatty <jermwatt@gmail.com>
2026-03-23 14:21:30 -0700

    chore: add husky and commitlint for conventional commits

---
+++ commitlint.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'security', 'docs', 'test', 'chore', 'refactor', 'ci', 'style', 'build', 'revert']],
+  },
+}
